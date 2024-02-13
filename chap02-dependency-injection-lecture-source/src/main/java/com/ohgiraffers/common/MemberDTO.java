6ad77e0c1b9185425feece3b2c514e831df07a0c
yunjaeeun <yunjaeeun12@naver.com>
2024-02-13 12:35:45 +0900

    24.02.13 공부내용
    Springboot DI(Dependency Injection)

---
+++ chap02-dependency-injection-lecture-source/src/main/java/com/ohgiraffers/common/MemberDTO.java
@@ -0,0 +1,16 @@
+package com.ohgiraffers.common;
+
+import lombok.*;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+@ToString
+public class MemberDTO {
+    private int sequence;            // 회원번호
+    private String name;             // 이름
+    private String phone;            // 휴대폰 번호
+    private String email;            // 이메일
+    private Account personalAccount; // 개인계좌
+}
