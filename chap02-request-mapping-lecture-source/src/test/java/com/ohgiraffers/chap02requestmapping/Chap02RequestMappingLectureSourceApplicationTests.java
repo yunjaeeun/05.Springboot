5731a45ef9fb8a55f266fa71bca2dc669606f58f
yunjaeeun <yunjaeeun12@naver.com>
2024-02-15 17:45:20 +0900

    24.02.15 공부내용
    Class handler, method hander

---
+++ chap02-request-mapping-lecture-source/src/test/java/com/ohgiraffers/chap02requestmapping/Chap02RequestMappingLectureSourceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ohgiraffers.chap02requestmapping;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Chap02RequestMappingLectureSourceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
