2ddc360312666a93f07b84dcebb15dd2cb5f35b9
yunjaeeun <yunjaeeun12@naver.com>
2024-02-15 11:28:24 +0900

    레포 변경

--- chap03-id-annotation-lecture-source/src/main/java/com/ohgiraffers/section02/annotation/common/Pokemon.java
+++
@@ -1,7 +0,0 @@
-package com.ohgiraffers.section02.annotation.common;
-
-public interface Pokemon {
-
-    /* 설명. 공격 기능 */
-    void attack();
-}
--- chap05-aop-lecture-source/src/main/java/com/ohgiraffers/section01/aop/ContextConfigurator.java
+++
@@ -1,9 +0,0 @@
-package com.ohgiraffers.section01.aop;
-
-import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.EnableAspectJAutoProxy;
-
-@Configuration
-@EnableAspectJAutoProxy(proxyTargetClass = true)        // @Aspect를 키고 끌지 결정
-public class ContextConfigurator {
-}
